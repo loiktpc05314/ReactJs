d8030d9b3071a1faba186173c5baccb50e2b5ec2
hieuM642004 <136356604+hieuM642004@users.noreply.github.com>
2024-07-18 17:35:21 +0700

    Rename menu

--- frontend/src/Components/Layouts/DefaultLayout/NavBar/NavBar.jsx
+++ frontend/src/Components/Layouts/DefaultLayout/NavBar/NavBar.jsx
@@ -144,7 +144,7 @@ function Navbar() {
 								to="/"
 								className="block py-2 px-3  bg-blue-700 rounded md:bg-transparent text-gray-900 md:p-0 "
 							>
-								Trang chủ
+								Home
 							</Link>
 						</li>
 						<li>
@@ -152,7 +152,7 @@ function Navbar() {
 								to="/authors"
 								className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 							>
-								Tác giả
+								Questions
 							</Link>
 						</li>
 						<li>
@@ -160,7 +160,15 @@ function Navbar() {
 								to="/posts"
 								className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 							>
-								Bài viết
+								Tags
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="/posts"
+								className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+							>
+								Users
 							</Link>
 						</li>
 					</ul>
